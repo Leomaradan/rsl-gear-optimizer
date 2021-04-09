b74714d768af82d2f76c9618246d6cc5b9337e57
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2021-04-09 08:20:21 +0200

    Add PHP backend starter

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-const { src, dest } = require('gulp');
+const { src, dest } = require("gulp");
 
 const copyRaidData = () => {
-  return src('node_modules/raid-data/images/**/*.png')
-    .pipe(dest('public/assets/'));
-}
+  return src("node_modules/raid-data/images/**/*.png").pipe(
+    dest("public/assets/")
+  );
+};
+
+const copyBackend = () => {
+  return src("backend/index.php").pipe(dest("build/"));
+};
 
 exports.copyRaidData = copyRaidData;
+exports.copyBackend = copyBackend;
